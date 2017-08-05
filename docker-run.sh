3b47d842d0d078d9e334064df3f78e3dc0283f10
infobarbosa <infobarbosa@yahoo.com.br>
2017-08-05 14:43:58 -0300

    Ajustados parametros de conectividade com o Kafka. Incluidos os parametros de rede docker.

--- docker-run.sh
+++ docker-run.sh
@@ -1,10 +1,12 @@
 docker run -d \
     --name twitter2kafka \
-    --env KAFKA_BOOTSTRAP_SERVERS="172.17.0.3:9092" \
+    --ip 172.18.0.5 \
+    --net bigdata \
+    --env KAFKA_BOOTSTRAP_SERVERS="172.17.0.4:9092" \
     --env KAFKA_TOPIC="tweets" \
     --env TWITTER_HASHTAG="#GameOfThrones" \
     --env TWITTER_CONSUMER_KEY=$TWITTER_CONSUMER_KEY \
     --env TWITTER_CONSUMER_SECRET=$TWITTER_CONSUMER_SECRET \
     --env TWITTER_ACCESS_TOKEN=$TWITTER_ACCESS_TOKEN \
     --env TWITTER_ACCESS_TOKEN_SECRET=$TWITTER_ACCESS_TOKEN_SECRET \
-	infobarbosa/twitter2kafka:1.0-SNAPSHOT
+    infobarbosa/twitter2kafka:1.0-SNAPSHOT
