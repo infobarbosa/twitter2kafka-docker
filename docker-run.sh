d65f2f23d046cf2521d32ee6db52ed0073342dad
infobarbosa <infobarbosa@yahoo.com.br>
2017-08-06 21:22:20 -0300

    ajustado o ip do kafka para a subnet bigdata

--- docker-run.sh
+++ docker-run.sh
@@ -2,7 +2,7 @@ docker run -d \
     --name twitter2kafka \
     --ip 172.18.0.5 \
     --net bigdata \
-    --env KAFKA_BOOTSTRAP_SERVERS="172.17.0.4:9092" \
+    --env KAFKA_BOOTSTRAP_SERVERS="172.18.0.4:9092" \
     --env KAFKA_TOPIC="tweets" \
     --env TWITTER_HASHTAG="#GameOfThrones" \
     --env TWITTER_CONSUMER_KEY=$TWITTER_CONSUMER_KEY \
